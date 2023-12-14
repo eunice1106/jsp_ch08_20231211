e17d6b07feae40e4753a397ff7ca4a66f1eaa66f
eunice1106 <149341156+eunice1106@users.noreply.github.com>
2023-12-14 23:57:46 +0900

    forward

---
+++ jsp_chapter08_20231211/src/main/java/com/eunice/test/MainClass.java
@@ -0,0 +1,17 @@
+package com.eunice.test;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student = new Student("홍길동", 27, 3);
+		
+		//student.name = "홍길동";
+		student.setName("홍길동");
+		student.setAge(27);
+		student.setGrade(3);
+		
+		System.out.println(student.getName());
+	}
+
+}
